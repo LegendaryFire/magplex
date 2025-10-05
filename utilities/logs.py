659028acf650a520df4d70fe8f2ad9096889424d
LegendaryFire <tristan.balon@outlook.com>
2025-10-05 01:19:26 -0700

    Remove unused log module

--- utilities/logs.py
+++
@@ -1,8 +0,0 @@
-from datetime import datetime
-
-
-def dump_error(data):
-    timestamp = datetime.now().strftime("%Y%m%d_%H%M%S")
-    filename = f"logs/{timestamp}.data"
-    with open(filename, "a") as file:
-        file.write(data)
